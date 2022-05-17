9ea88de88ede47afec2e4234e2fb86bd9e3fe0d9
DESIREE LARISSA MARINEZ JIMENEZ <1101374@est.intec.edu.do>
2022-05-17 15:35:13 -0400

    Hyphen validation ready.

--- SSN Validator/Program.cs
+++ SSN Validator/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello World!");
+﻿string input;
+Console.WriteLine("Enter a string: ");
+input = Console.ReadLine();
+
+string[] hyphen = input.Split("-");
+
+if (hyphen[0].Length == 3 && hyphen[1].Length == 2 && hyphen[2].Length == 4)
+{
+    Console.WriteLine("Valid.");
+}
+else
+    Console.WriteLine("Invalid.");
