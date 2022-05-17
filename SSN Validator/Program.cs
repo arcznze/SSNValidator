37254b4ac7aeb6de432d5e8242787ac23a0eaa72
DESIREE LARISSA MARINEZ JIMENEZ <1101374@est.intec.edu.do>
2022-05-17 16:02:57 -0400

    Validation ready.

--- SSN Validator/Program.cs
+++ SSN Validator/Program.cs
@@ -1,11 +1,13 @@
-﻿string input;
+﻿using System.Text.RegularExpressions;
+
+string input;
 string[] x = new string[3];
 string[] y = new string[2];
 string[] z = new string[4];
 
 Console.WriteLine("Enter a string: ");
 input = Console.ReadLine();
-
+Regex regex = new Regex(@"^\d+$");
 string[] hyphen = input.Split("-");
 
 if (hyphen[0].Length == 3 && hyphen[1].Length == 2 && hyphen[2].Length == 4)
@@ -20,20 +22,19 @@ void GetDigits()
     {
         x[i] = input.Substring(i, 1);
     }
-    for (int i = 0; i < 2; i++)
-    {
-        y[i] = input.Substring(i, 1);
-    }
-    for (int i = 0; i < 4; i++)
-    {
-        z[i] = input.Substring(i, 1);
-    }
     Valid();
 }
 void Valid()
 {
-    if (int.Parse(x[0]) == 0 && int.Parse(x[1]) == 0 && int.Parse(x[2]) == 0 || int.Parse(x[0]) == 6 && int.Parse(x[1]) == 6 && int.Parse(x[2]) == 6 || int.Parse(hyphen[0]) >= 900)
-        Console.WriteLine("Invalid.");
+    if (regex.IsMatch(hyphen[0]) && regex.IsMatch(hyphen[1]) && regex.IsMatch(hyphen[2]))
+    {
+        if (int.Parse(x[0]) == 0 && int.Parse(x[1]) == 0 && int.Parse(x[2]) == 0 || int.Parse(x[0]) == 6 && int.Parse(x[1]) == 6 && int.Parse(x[2]) == 6 || int.Parse(hyphen[0]) >= 900)
+            Console.WriteLine("Invalid.");
+        else if (hyphen[2] == "00" || hyphen[2] == "0000")
+            Console.WriteLine("Invalid.");
+        else
+            Console.WriteLine("Valid.");
+    }
     else
-        Console.WriteLine("Valid.");
+        Console.WriteLine("Invalid.");
 }
